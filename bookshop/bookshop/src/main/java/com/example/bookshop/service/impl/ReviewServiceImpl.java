557d2b80ad7d207aad993004d0f6a96d1b78b1cc
huynhpx736 <92499389+huynhpx736@users.noreply.github.com>
2024-09-07 20:03:50 +0700

    fix review

--- bookshop/bookshop/src/main/java/com/example/bookshop/service/impl/ReviewServiceImpl.java
+++ bookshop/bookshop/src/main/java/com/example/bookshop/service/impl/ReviewServiceImpl.java
@@ -1,14 +1,19 @@
 package com.example.bookshop.service.impl;
 
 import com.example.bookshop.dto.ReviewDTO;
+import com.example.bookshop.entity.Product;
 import com.example.bookshop.entity.Review;
+import com.example.bookshop.entity.User;
 import com.example.bookshop.mapper.ReviewMapper;
+import com.example.bookshop.repository.ProductRepository;
 import com.example.bookshop.repository.ReviewRepository;
+import com.example.bookshop.repository.UserRepository;
 import com.example.bookshop.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,6 +24,19 @@ public class ReviewServiceImpl implements ReviewService {
 
     @Autowired
     private ReviewMapper reviewMapper;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<ReviewDTO> getAllReviewsByBookId(int bookId) {
+        return reviewRepository.findAllByProductId(bookId).stream()
+                .map(reviewMapper::toDTO)
+                .collect(Collectors.toList());
+    }
 
     @Override
     public List<ReviewDTO> getAllReviews() {
@@ -37,7 +55,15 @@ public class ReviewServiceImpl implements ReviewService {
     @Override
     public ReviewDTO createReview(ReviewDTO reviewDTO) {
         Review review = reviewMapper.toEntity(reviewDTO);
+        if (reviewDTO.getProductId() == null || reviewDTO.getUserId() == null) return null;
+        Optional<Product> product = productRepository.findById(reviewDTO.getProductId());
+        Optional<User> user = userRepository.findById(reviewDTO.getUserId());
+        if (product.isEmpty() || user.isEmpty()) return null;
+        review.setProduct(product.get());
+        review.setUser(user.get());
+
         return reviewMapper.toDTO(reviewRepository.save(review));
+
     }
 
     @Override
